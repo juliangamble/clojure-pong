8cdd9723cac13dd4ae1449652b55d8f874d1b9e4
canassa <devnull@localhost>
2011-02-20 18:27:47 -0300

    Uses globals to keep the window size

--- pong.clj
+++ pong.clj
@@ -1,10 +1,14 @@
 (import (javax.swing JFrame)
         (java.awt Dimension))
 
+; The window size
+(def width 400)
+(def height 400)
+
 (defn main []
     (let [frame (new JFrame)]
         (.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
-        (.setSize frame (new Dimension 400 400))
+        (.setSize frame (new Dimension width height))
         (.show frame)))
 
 (main)
