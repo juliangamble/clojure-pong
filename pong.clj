9c9f639161ffc83b51d297bce434efd2857fa864
canassa <devnull@localhost>
2011-02-20 20:55:14 -0300

    Implemented a game loop

--- pong.clj
+++ pong.clj
@@ -11,11 +11,17 @@
 (def width 400)
 (def height 400)
 
-(defn drawn [frame]
+(defn drawn [frame time]
     (let [buffer (.getBufferStrategy frame)
           graphics (.getDrawGraphics buffer)]
+
+        ; Clears the screen
         (.setColor graphics Color/BLACK)
-        (.drawLine graphics 0 0 100 100)
+        (.fillRect graphics 0 0 width height)
+
+        ; Draw a ball at time/50
+        (.setColor graphics Color/WHITE)
+        (.drawOval graphics 200 (/ time 50) 10 10)
 
         ; It is best to dispose() a Graphics object when done with it.
         (.dispose graphics) 
@@ -24,7 +30,8 @@
         (.show buffer)))
 
 (defn main []
-    (let [frame (new JFrame "Clojure Pong")]
+    (let [frame (new JFrame "Clojure Pong")
+          start-time (System/currentTimeMillis)]
         (.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
         (.setSize frame (new Dimension width height))
         (.setUndecorated frame true)
@@ -33,6 +40,10 @@
         (.createBufferStrategy frame 2)
         (.show frame)
 
-        (drawn frame)))
+        (loop [time 0]
+            (println time)
+            (drawn frame time)
+            (Thread/sleep 50)
+            (recur (- (System/currentTimeMillis) start-time)))))
 
 (main)
