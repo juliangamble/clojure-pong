b3d4283f9c3939e7813652abd3777243a2bddb1a
canassa <devnull@localhost>
2011-02-21 01:35:15 -0300

    More references added

--- pong.clj
+++ pong.clj
@@ -3,6 +3,7 @@
 ; References:
 ; http://gpwiki.org/index.php/Java:Tutorials:Double_Buffering
 ; http://zetcode.com/tutorials/javagamestutorial/
+; http://jng.imagine27.com/articles/2009-09-12-122605_pong_in_clojure.html
 
 (import (javax.swing JFrame)
         (java.awt Color Dimension)
