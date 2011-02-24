061199ab51b71c405a07e9eadf8eb1e714c3ff34
Julio Nobrega Netto <inerte@gmail.com>
2011-02-24 07:54:44 -0300

    Improved comments

--- pong.clj
+++ pong.clj
@@ -1,19 +1,19 @@
-; The pong in Clojure
-;
-; References:
-; http://gpwiki.org/index.php/Java:Tutorials:Double_Buffering
-; http://zetcode.com/tutorials/javagamestutorial/
-; http://jng.imagine27.com/articles/2009-09-12-122605_pong_in_clojure.html
+;; The pong in Clojure
+;;
+;; References:
+;; http://gpwiki.org/index.php/Java:Tutorials:Double_Buffering
+;; http://zetcode.com/tutorials/javagamestutorial/
+;; http://jng.imagine27.com/articles/2009-09-12-122605_pong_in_clojure.html
 
 (import (javax.swing JFrame)
         (java.awt Color Dimension Toolkit)
         (java.awt.event KeyListener))
 
-; The window size
+;; The window size
 (def window-width 600)
 (def window-height window-width)
 
-; The court size: 23.78m by 8.23m, which gives a proportion of 2,88
+;; A tennis court size: 23.78m by 8.23m, which gives a proportion of 2,88
 (def court-width window-width)
 (def bleacher-height (- window-width (/ window-width 2.88)))
 (def court-height bleacher-height)
@@ -24,7 +24,7 @@
 
 (def new-ball {:x 200 :y 200 :sx 0.1 :sy 0})
 
-; Defines a atom to store the rackets positions
+;; Defines a atom to store the rackets positions
 (def racquet-left-position (atom (/ window-height 2)))
 (def racquet-right-position (atom (/ window-height 2)))
 
@@ -42,7 +42,7 @@
         (colision-y? ball) (merge ball {:y (- window-width 10) :sy (* -1 (ball :sy))})
         (colision-xr? ball) (merge ball {:x (- window-height 10) :sx (* -1 (ball :sx))})
         (colision-xl? ball) (merge ball {:x 10 :sx (* -1 (ball :sx))})
-        ; Apply the physics, I added +1 in the step in order to avoid division by zero
+        ;; Apply the physics, I added +1 in the step in order to avoid division by zero
         :else (merge ball {:x (+ (ball :x) (* (+ step 1) (ball :sx)))
                            :y (+ (ball :y) (* (+ step 1) (ball :sy)))
                            :sy (+ (ball :sy) (* 0.000098 step))})))
@@ -51,26 +51,26 @@
     (let [buffer (.getBufferStrategy frame)
           graphics (.getDrawGraphics buffer)]
 
-        ; Clears the screen
+        ;; Clears the screen
         (.setColor graphics Color/BLACK)
         (.fillRect graphics 0 0 window-width window-height)
 
-        ; Draw the ball
+        ;; Draw the ball
         (.setColor graphics Color/WHITE)
         (.drawOval graphics (ball :x) (ball :y) 10 10)
 
         (.fillRect graphics 0 (- window-height court-height 5) court-width 5)
 
-        ; Draw the left racket
+        ;; Draw the left racket
         (.fillRect graphics 5 (- @racquet-left-position racquet-middle-height) racquet-width racquet-height)
 
-        ; Draw the right racket
+        ;; Draw the right racket
         (.fillRect graphics (- window-width (+ racquet-width 5)) (- @racquet-right-position racquet-middle-height) racquet-width racquet-height)
 
-        ; It is best to dispose() a Graphics object when done with it.
+        ;; It is best to dispose() a Graphics object when done with it.
         (.dispose graphics)
 
-        ; Shows the contents of the backbuffer on the screen.
+        ;; Shows the contents of the backbuffer on the screen.
         (.show buffer)))
 
 (defn main []
@@ -86,16 +86,16 @@
 
         (.addKeyListener frame (proxy [KeyListener] []
             (keyPressed [e]
-                ; Exits when 'q' is pressed
+                ;; Exits when 'q' is pressed
                 (if (= (.getKeyChar e) \q) (System/exit 0) )
 
-                ; Pressing 'a' or 'z' updates the left racket position
+                ;; Pressing 'a' or 'z' updates the left racket position
                 (if (and (< @racquet-left-position (- window-height racquet-middle-height)) (= (.getKeyChar e) \z))
                     (swap! racquet-left-position + 5))
                 (if (and (> @racquet-left-position 25) (= (.getKeyChar e) \a))
                     (swap! racquet-left-position - 5))
 
-                ; Pressing 'j' or 'm' updates the right racket position
+                ;; Pressing 'j' or 'm' updates the right racket position
                 (if (and (< @racquet-right-position (- window-height racquet-middle-height)) (= (.getKeyChar e) \m))
                     (swap! racquet-right-position + 5))
                 (if (and (> @racquet-right-position 25) (= (.getKeyChar e) \j))
@@ -104,7 +104,7 @@
             (keyReleased [e])
             (keyTyped [e])))
 
-        ; Makes sure everything inside the frame fits
+        ;; Makes sure everything inside the frame fits
         (.validate frame)
 
         (.show frame)
@@ -115,8 +115,8 @@
 
                 (println ball)
 
-                ; Since Clojure is so fast, a sleep is "required" in order to avoid 0ms time steps
-                ; We need to implement a better game loop (Threads?)
+                ;; Since Clojure is so fast, a sleep is "required" in order to avoid 0ms time steps
+                ;; We need to implement a better game loop (Threads?)
                 (Thread/sleep 20)
 
                 (recur (System/currentTimeMillis) time (update-ball ball step))))))
